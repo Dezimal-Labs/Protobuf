8ca3de6405dc1f757a567bb95bf3e714890b2764
jabeermk <jabeermk@gmail.com>
2021-12-07 15:57:29 +0530

    First

---
+++ ProductGrpc/Data/ProductsContextSeed.cs
@@ -0,0 +1,49 @@
+﻿using ProductGrpc.Models;
+
+namespace ProductGrpc.Data
+{
+    public class ProductsContextSeed
+    {
+        public static void SeedAsync(ProductsContext productsContext)
+        {
+            if(!productsContext.Products.Any())
+            {
+                var products = new List<Product>
+                {
+                    new Product {
+                        ProductId = 1,
+                        Name ="Mi10T",
+                        Description ="New Xiomi Phone Mi10T",
+                        Price =699,
+                        status =ProductGrpc.Models.ProductStatus.INSTOCK,
+                        CreatedTime = DateTime.UtcNow
+                        },
+
+                      new Product {
+                        ProductId = 2,
+                        Name ="Mi10T",
+                        Description ="New Xiomi Phone Mi10T",
+                        Price =699,
+                        status =ProductGrpc.Models.ProductStatus.INSTOCK,
+                        CreatedTime = DateTime.UtcNow
+                        },
+
+                    new Product {
+                        ProductId = 3,
+                        Name ="A50",
+                        Description ="New Samsung Phone A50",
+                        Price =699,
+                        status =ProductGrpc.Models.ProductStatus.INSTOCK,
+                        CreatedTime = DateTime.UtcNow
+                        }
+                  
+                };
+
+                productsContext.Products.AddRange(products);    
+                productsContext.SaveChanges();
+            }
+        }
+
+       
+    }
+}
---
+++ ProductGrpc/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using ProductGrpc.Data;
+using ProductGrpc.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+
+builder.Services.AddTransient<ProductsContextSeed>();
+// Add services to the container.
+builder.Services.AddGrpc();
+builder.Services.AddDbContext<ProductsContext>(options => options.UseInMemoryDatabase("Products"));
+var app = builder.Build();
+
+SeedDatabase(app);
+
+void SeedDatabase(IHost app)
+{
+    //var scopedFactory=app.Services.GetService<IServiceScopeFactory>();
+    //using (var scope = scopedFactory.CreateScope())
+    //{
+    //    var service = scope.ServiceProvider.GetService<ProductsContextSeed>();
+        
+    //    service.SeedAsync()
+    //}
+
+    using var scope=app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var productContext=services.GetRequiredService<ProductsContext>();
+    ProductsContextSeed.SeedAsync(productContext);
+}
+
+
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
